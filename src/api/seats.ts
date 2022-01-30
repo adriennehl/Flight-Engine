a29924ed9dea9f48ad49ffe37035eda8efd96d53
adriennehl <al81@rice.edu>
2022-01-29 19:05:47 -0600

    rec endpoint

--- src/api/seats.ts
+++ src/api/seats.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 
 export const seats = Router();
-
 seats.get('/', (req, res) => {
   const { query } = req;
   if (!query || !query.flightNumber) {
@@ -10,8 +9,41 @@ seats.get('/', (req, res) => {
   }
 
   const {flightNumber} = query;
+  res.json(generatedSeats);
+});
 
-  const generatedSeats = { available: [{row: 5, seat: 'B', type: 'middle', class: 'first', price: 149}, 
+seats.get('/rec', (req, res) => {
+  const { query } = req;
+  const { priceMin, priceMax, preferences} = query;
+  const min = priceMin && typeof priceMin == "string" ? parseInt(priceMin) : 0
+  const max = priceMax && typeof priceMax == "string" ? parseInt(priceMax) : Infinity
+  // get affordable seats
+  const affordableSeats = generatedSeats.available.filter((seat) => {
+    return seat.price >= min && seat.price <= max
+  })
+  // find pairs of seats
+  let pairs: {row: number, seat: string, type: string, class: string, price: number}[][] = [];
+  for (let i = 0; i < affordableSeats.length - 1; i++) {
+    if (affordableSeats[i].row == affordableSeats[i+1].row &&
+      Math.abs(seatToNum(affordableSeats[i].seat) - seatToNum(affordableSeats[i+1].seat)) == 1) {
+        pairs.push([affordableSeats[i], affordableSeats[i+1]])
+      }
+  }
+
+  res.json(pairs.slice(0,5));
+})
+
+const seatToNum = (seat: string) => {
+  if (seat == "A") return 1; 
+  else if (seat == "B") return 2;
+  else if (seat == "C") return 3;
+  else if (seat == "D") return 5;
+  else if (seat == "E") return 6;
+  else if (seat == "F") return 7;
+  else return -1;
+}
+
+const generatedSeats = { available: [{row: 5, seat: 'B', type: 'middle', class: 'first', price: 149}, 
     {row: 5, seat: 'C', type: 'aisle', class: 'first', price: 149},
     {row: 7, seat: 'A', type: 'window', class: 'first', price: 149},
     {row: 7, seat: 'D', type: 'aisle', class: 'first', price: 149},
@@ -19,14 +51,14 @@ seats.get('/', (req, res) => {
     {row: 7, seat: 'F', type: 'window', class: 'first', price: 149},
     {row: 9, seat: 'B', type: 'middle', class: 'first', price: 149},
     {row: 9, seat: 'C', type: 'aisle', class: 'first', price: 149},
-    {row: 13, seat: 'B', type: 'middle', class: 'preferred', price: 105},
-    {row: 13, seat: 'C', type: 'aisle', class: 'preferred', price: 105},
-    {row: 13, seat: 'D', type: 'aisle', class: 'preferred', price: 105},
-    {row: 13, seat: 'E', type: 'middle', class: 'preferred', price: 105},
-    {row: 14, seat: 'B', type: 'middle', class: 'preferred', price: 105},
-    {row: 14, seat: 'C', type: 'aisle', class: 'preferred', price: 105},
-    {row: 15, seat: 'D', type: 'aisle', class: 'preferred', price: 105},
-    {row: 15, seat: 'E', type: 'middle', class: 'preferred', price: 105},
+    {row: 13, seat: 'B', type: 'middle', class: 'preferred', price: 125},
+    {row: 13, seat: 'C', type: 'aisle', class: 'preferred', price: 125},
+    {row: 13, seat: 'D', type: 'aisle', class: 'preferred', price: 125},
+    {row: 13, seat: 'E', type: 'middle', class: 'preferred', price: 125},
+    {row: 14, seat: 'B', type: 'middle', class: 'preferred', price: 115},
+    {row: 14, seat: 'C', type: 'aisle', class: 'preferred', price: 115},
+    {row: 15, seat: 'D', type: 'aisle', class: 'preferred', price: 115},
+    {row: 15, seat: 'E', type: 'middle', class: 'preferred', price: 115},
     {row: 17, seat: 'B', type: 'middle', class: 'preferred', price: 105},
     {row: 17, seat: 'C', type: 'aisle', class: 'preferred', price: 105},
     {row: 18, seat: 'D', type: 'aisle', class: 'preferred', price: 105},
@@ -47,9 +79,7 @@ seats.get('/', (req, res) => {
     {row: 29, seat: 'B', type: 'middle', class: 'economy', price: 55},
     {row: 31, seat: 'B', type: 'middle', class: 'economy', price: 55},
     {row: 31, seat: 'C', type: 'aisle', class: 'economy', price: 55},
-    {row: 33, seat: 'D', type: 'aisle', class: 'economy', price: 55},
-    {row: 33, seat: 'E', type: 'middle', class: 'economy', price: 55},
-    {row: 36, seat: 'A', type: 'window', class: 'economy', price: 55},
-    {row: 36, seat: 'B', type: 'middle', class: 'economy', price: 55}]}
-  res.json(generatedSeats);
-});
+    {row: 33, seat: 'D', type: 'aisle', class: 'economy', price: 45},
+    {row: 33, seat: 'E', type: 'middle', class: 'economy', price: 45},
+    {row: 36, seat: 'A', type: 'window', class: 'economy', price: 45},
+    {row: 36, seat: 'B', type: 'middle', class: 'economy', price: 45}]}
