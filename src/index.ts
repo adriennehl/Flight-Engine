e85a06b0a0b9367692ce86d772c1e78989eb57f0
adriennehl <al81@rice.edu>
2022-01-30 05:19:32 -0600

    env variables

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,23 @@ const port = env.port || '4000';
 
 const app = express();
 
+require('dotenv').config()
+
 // firebase set up
 const admin = require("firebase-admin");
-const serviceAccount = require("./travel-buddy-7df44-firebase-adminsdk-daz2j-9d4c3116d6.json");
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+  "type": process.env.type,
+  "project_id": process.env.project_id,
+  "private_key_id": process.env.private_key_id,
+  "private_key": process.env.private_key,
+  "client_id": process.env.client_id,
+  "client_email": process.env.client_email,
+  "auth_uri": process.env.auth_uri,
+  "token_uri": process.env.token_uri,
+  "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+  "client_x509_cert_url": process.env.client_x509_cert_url
+}),
   databaseURL: "https://travel-buddy-7df44-default-rtdb.firebaseio.com"
 });
 export const db = admin.database();
